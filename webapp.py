5dc67017879e11b240e605b7b17943fb8d9e9f89
EliGrosman <eligrosman1@gmail.com>
2020-08-31 18:33:23 -0700

    Cleanup

--- webapp.py
+++ webapp.py
@@ -7,133 +7,62 @@ import json
 import random
 import string
 
-UPLOAD_FOLDER = './upload/'
 CONVERT_FOLDER = './convert/'
 CLICKS_FOLDER = './clicks/'
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['CONVERT_FOLDER'] = CONVERT_FOLDER
 app.config['CLICKS_FOLDER'] = CLICKS_FOLDER
+
+
+
+# ---- Endpoints for Web App ----
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
+
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
+
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+
         if file and allowed_file(file.filename):
+
             filename = secure_filename(file.filename)
             freq = float(request.form['freq'])
             duration = float(request.form['duration'])
             vol_song = (int(request.form['vol_song']))/100
             vol_click = (int(request.form['vol_click']))/100
+
             retFile = generate_click(file, filename, freq, duration, vol_song, vol_click, app.config['CONVERT_FOLDER'])
-            return redirect(url_for('uploaded_file', filename = retFile))
+
+            return redirect(url_for('return_file', filename = retFile))
+            
     return render_template('home.html')
 
+@app.route('/generated/<filename>')
+def return_file(filename):
+  return send_from_directory(os.path.join(app.config['CONVERT_FOLDER'], filename))
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-  return send_from_directory(app.config['CONVERT_FOLDER'],
-                            filename)
 
-@app.route('/convert', methods = ['POST'])
-def convert():
-  if 'audioFile' not in request.files:
-    return render_template("noAudioFile.html"), 400
-  file = request.files['audioFile']
-  if file and allowed_file(file.filename):
-    converted, sr, newName = convert_file(file, file.filename, file.filename + "_converted.wav", app.config['CONVERT_FOLDER'])
-    response = make_response(send_file(os.path.join(app.config['CONVERT_FOLDER'], newName), attachment_filename = "converted.wav", as_attachment = True))
-    return response, 200
-  else:
-    return render_template('badFileType.html'), 400
 
-@app.route('/generate', methods = ['POST'])
-def generate():
-  if 'audioFile' not in request.files:
-    return render_template("noAudioFile.html"), 400
-  file = request.files['audioFile']
-  if file and allowed_file(file.filename):
-    converted, sr, newName = convert_file(file, file.filename, file.filename + "_converted.wav", app.config['CONVERT_FOLDER'])
-    click_freq = 880
-    click_duration = 0.5
-    vol_adj_song = 1
-    vol_adj_click = 1
-    retName = generate_click_only(converted, file.filename, file.filename + "_clicks.wav", click_freq, click_duration, app.config['CLICKS_FOLDER'])
-    response = make_response(send_file(os.path.join(app.config['CLICKS_FOLDER'], retName), attachment_filename = "clicks.wav", as_attachment = True))
-    return response, 200
-  else:
-    return render_template('badFileType.html'), 400
+# ---- Endpoint for Android App ----
+
+
 
 @app.route('/generateFull', methods = ['POST'])
 def generateFull():
   if 'audioFile' not in request.files:
     return render_template("noAudioFile.html"), 400
-  file = request.files['audioFile']
-  if file and allowed_file(file.filename):
-    converted, sr, newName = convert_file(file, file.filename, file.filename + "_converted.wav_", app.config['CONVERT_FOLDER'])
-    _, beats = generate_beats(converted, sr)
-    ret = {
-      "beats": beats.tolist(),
-      "sr": sr,
-    }
-    response = make_response(send_file(os.path.join(app.config['CONVERT_FOLDER'], newName), attachment_filename = "converted.wav", as_attachment = True))
-    response.headers['X-Beats'] = ret
-    return response, 200
-  else:
-    return render_template('badFileType.html'), 400
-
-
-@app.route('/generateTwo', methods = ['POST'])
-def generateTwo():
-  if 'audioFile' not in request.files:
-    return render_template("noAudioFile.html"), 400
-
-  click_freq = request.args.get("click_freq")
-  click_dur = request.args.get("click_dur")
-
-  if click_freq is None or click_dur is None:
-    return render_template('badFileType.html'), 400
 
   file = request.files['audioFile']
+
   if file and allowed_file(file.filename):
-    letters = string.ascii_lowercase
-    hashName = ''.join(random.choice(letters) for i in range(16))
 
-    converted, sr, newName = convert_file(file, file.filename, hashName + "_converted.wav", app.config['CONVERT_FOLDER'])
-    retName = generate_click_only(converted, file.filename, hashName + "_clicks.wav", float(click_freq), float(click_dur), app.config['CLICKS_FOLDER'])
-
-    response = make_response()
-    response.headers['X-Urls'] = { "key": hashName }
-    return response, 200
-  else: 
-    return render_template('badFileType.html'), 400
-
-@app.route('/get/<filename>')
-def converted_file(filename):
-  if '_' in filename:
-    ext = filename.rsplit('_', 1)[1].lower()
-    if(ext == "converted.wav"):
-      return send_from_directory(app.config['CONVERT_FOLDER'], filename)
-    elif(ext == "clicks.wav"):
-      return send_from_directory(app.config['CLICKS_FOLDER'], filename)
-
-
-@app.route('/generateMixed', methods = ['POST'])
-def generateMixed():
-  if 'audioFile' not in request.files:
-    return render_template("noAudioFile.html"), 400
-  file = request.files['audioFile']
-  if file and allowed_file(file.filename):
     click_freq = request.args.get("click_freq")
     click_dur = request.args.get("click_dur")
 
@@ -141,11 +70,14 @@ def generateMixed():
       return render_template('badFileType.html'), 400
 
     saveName = generate_click(file, file.filename, float(click_freq), float(click_dur), 1, 1, app.config['CONVERT_FOLDER'])
-    print(saveName)
+
     response = make_response(send_file(os.path.join(app.config['CONVERT_FOLDER'], saveName), attachment_filename = "converted.wav", as_attachment = True))
+    
     return response, 200
+  
   else:
     return render_template('badFileType.html'), 400
+
 
 
 app.run(host = "0.0.0.0")
